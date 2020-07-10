aca687bfd4f0d144c7ff5db4e7efa6ad47c30f25
alexonbstudio <alexonbstudio@gmail.com>
2020-07-10 02:00:09 +0200

    Update brave-rewards-verification.php

--- themes/seo/txt/brave-rewards-verification.php
+++ themes/seo/txt/brave-rewards-verification.php
@@ -1,4 +1,4 @@
-<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<?php header('Content-Type: text/plain; charset=utf-8'); ?>
 This is a Brave Rewards publisher verification file.
 
 Domain: <?php echo $sites['domain']; ?>
